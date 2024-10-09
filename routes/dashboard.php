a04dfdb01bfef4c54c30bf3a342afce422c0f0ff
DinmaOtutu <otutudinma1995@gmail.com>
2024-10-09 16:56:46 +0200

    dashboard routing

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\SubscriberController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
